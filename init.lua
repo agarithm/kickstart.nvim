7c15973e5a371fb83d7b99ba822348ec2bb201ed
Mike Agar <mike@agarithm.com>
2024-10-05 10:40:17 -0400

    Adding avante cursor-like Claude plugin

--- init.lua
+++ init.lua
@@ -972,6 +972,98 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      -- add any opts here
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make BUILD_FROM_SOURCE=true',
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+          {
+            'yetone/avante.nvim',
+            event = 'VeryLazy',
+            lazy = false,
+            version = false, -- set this if you want to always pull the latest change
+            opts = {
+              -- add any opts here
+            },
+            -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+            build = 'make',
+            -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+            dependencies = {
+              'nvim-treesitter/nvim-treesitter',
+              'stevearc/dressing.nvim',
+              'nvim-lua/plenary.nvim',
+              'MunifTanjim/nui.nvim',
+              --- The below dependencies are optional,
+              'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+              'zbirenbaum/copilot.lua', -- for providers='copilot'
+              {
+                -- support for image pasting
+                'HakonHarnes/img-clip.nvim',
+                event = 'VeryLazy',
+                opts = {
+                  -- recommended settings
+                  default = {
+                    embed_image_as_base64 = false,
+                    prompt_for_file_name = false,
+                    drag_and_drop = {
+                      insert_mode = true,
+                    },
+                    -- required for Windows users
+                    use_absolute_path = true,
+                  },
+                },
+              },
+              {
+                -- Make sure to set this up properly if you have lazy=true
+                'MeanderingProgrammer/render-markdown.nvim',
+                opts = {
+                  file_types = { 'markdown', 'Avante' },
+                },
+                ft = { 'markdown', 'Avante' },
+              },
+            },
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
